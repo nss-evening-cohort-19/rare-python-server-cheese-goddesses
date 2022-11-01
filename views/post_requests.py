c4c1b7d2f6e539cf0c221517502d0f96375d03f6
Shalane Proctor <shalane088@gmail.com>
2022-10-31 22:12:30 -0500

    added delete to posts

--- views/post_requests.py
+++ views/post_requests.py
@@ -1,3 +1,12 @@
 import sqlite3
 import json
+from models import Post
 
+def delete_post(id):
+  """Delete Post"""
+  with sqlite3.connect("./db.sqlite3") as conn:
+    db_cursor = conn.cursor()
+    db_cursor.execute("""
+    DELETE from post
+    WHERE id = ?                
+    """, (id, ))
