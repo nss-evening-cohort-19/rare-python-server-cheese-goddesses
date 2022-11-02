c902bf55d436b14daace31b49e4a21994548e2b6
Krima Patel <patel.krima@hotmail.com>
2022-11-01 19:22:28 -0500

    wrote create posts function, checked imports and init file

--- views/post_requests.py
+++ views/post_requests.py
@@ -17,22 +17,15 @@ def create_post(new_post):
         
         db_cursor.execute("""
         INSERT INTO Post
-            ( name, breed, status, location_id, customer_id )
+            ( user_id, category_id, title, publication_date, image_url, content, approved )
         VALUES
-            ( ?, ?, ?, ?, ?);
+            ( ?, ?, ?, ?, ?, ?, ?);
         """, (new_post['user_id'], new_post['category_id'],
               new_post['title'], new_post['publication_date'],
               new_post['image_url'], new_post['content'], new_post['approved'], ))
-
-        # The `lastrowid` property on the cursor will return
-        # the primary key of the last thing that got added to
-        # the database.
+        
         id = db_cursor.lastrowid
 
-        # Add the `id` property to the animal dictionary that
-        # was sent by the client so that the client sees the
-        # primary key in the response.
         new_post['id'] = id
-
 
     return json.dumps(new_post)
