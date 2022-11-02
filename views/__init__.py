083a0b29d9c359d107c6ec7d184ad5c81bb3a8bc
Krima Patel <patel.krima@hotmail.com>
2022-11-01 19:08:15 -0500

    wrote create function for posts

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,2 @@
 from .post_requests import delete_post
+from .post_requests import create_post
--- views/post_requests.py
+++ views/post_requests.py
@@ -10,3 +10,29 @@ def delete_post(id):
     DELETE from post
     WHERE id = ?                
     """, (id, ))
+    
+def create_post(new_post):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        INSERT INTO Post
+            ( name, breed, status, location_id, customer_id )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_post['user_id'], new_post['category_id'],
+              new_post['title'], new_post['publication_date'],
+              new_post['image_url'], new_post['content'], new_post['approved'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_post['id'] = id
+
+
+    return json.dumps(new_post)
