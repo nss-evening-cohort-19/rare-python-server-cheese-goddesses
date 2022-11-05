71ee1f4f2e99e94018963a8851ad279654c8f720
Krima Patel <patel.krima@hotmail.com>
2022-11-05 09:46:52 -0500

    still working on fixing update_comment, need other functions to test though

--- views/comment_requests.py
+++ views/comment_requests.py
@@ -2,18 +2,27 @@ import sqlite3
 import json
 from models import Comment
 
+COMMENTS = [
+  {
+        "id": 1,
+        "post_id": 2,
+        "author_id": 5,
+        "content": "A New Book Release"
+  }
+]
+
 def update_comment(id, new_comment):
     with sqlite3.connect("./db.sqlite3") as conn:
         db_cursor = conn.cursor()
         
         db_cursor.execute("""
-        UPDATE Comment
+        UPDATE Comments
             SET
-                author_id = ?,
                 post_id = ?,
+                author_id = ?,
                 content = ?
         WHERE id = ?
-        """, (new_comment['author_id'], new_comment['post_id'],
+        """, (new_comment['post_id'], new_comment['author_id'],
               new_comment['content'], id, ))
 
         # Were any rows affected?
